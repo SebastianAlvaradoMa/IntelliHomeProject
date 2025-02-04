073ec470ef74811b7ae1d76875938450edc045bb
Sebas36762 <sebas4101p@estudiantec.cr>
2025-02-03 22:02:34 -0600

    update junit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 dependencies {
     testImplementation ("junit:junit:4.13.2") // Para JUnit 4
     testImplementation ("org.junit.jupiter:junit-jupiter:5.9.0")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.ext:junit:1.2.1")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
     testImplementation ("androidx.test:runner:1.4.0")
     testImplementation ("androidx.test.espresso:espresso-core:3.4.0")
