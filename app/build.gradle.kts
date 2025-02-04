85c2a5ee7af6dac912e3cb0221babdda1901b7ee
Sebas36762 <sebas4101p@estudiantec.cr>
2025-02-03 21:42:52 -0600

    upgrade sdk 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ android {
         baseline = file("lint-baseline.xml")
     }
     namespace = "com.example.registro"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.registro"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
